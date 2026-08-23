9ab2d6ae018ef6b256463eaac8883496727b4df3
Laura Vitória <lvsa@cesar.school>
2026-08-23 00:32:24 -0300

    Criar orquestrador.h com structs Job e Orquestrador

--- include/orquestrador.h
+++ include/orquestrador.h
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fila.h"
-#include <pthread.h>
+
+typedef struct Job {
+    int job_id;
+    int pid;
+    char *nome_tarefa;
+} Job;
 
 typedef struct Orquestrador {
-    Fila *fila;
-    pthread_mutex_t lock;
+    Tarefa tarefas[100];
+    int qtd_tarefas;
+    
+    Job jobs[100];
+    int qtd_jobs;
+    
+    char diretorio_trabalho[256];
 } Orquestrador;
