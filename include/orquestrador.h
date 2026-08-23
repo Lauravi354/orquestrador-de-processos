4a21fb2fa54f058ba1eb50e8524efcb31a5a8160
Laura Vitória <lvsa@cesar.school>
2026-08-23 12:05:07 -0300

    Completar orquestrador.h

--- include/orquestrador.h
+++ include/orquestrador.h
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "fila.h"
 
+
+typedef enum {
+    SEQUENCIAL,
+    PARALELO,
+    PIPE
+} TipoExecucao;
+
 typedef struct Job {
     int job_id;
     int pid;
@@ -17,3 +24,19 @@ typedef struct Orquestrador {
     
     char diretorio_trabalho[256];
 } Orquestrador;
+
+Orquestrador* criar_orquestrador();
+
+int cadastrar_tarefa(Orquestrador *orq, char *nome, char *programa, char **argumentos, int qtd_args);
+
+int executar(Orquestrador *orq, TipoExecucao tipo, char **nomes_tarefas, int qtd_tarefas);
+
+int mudar_diretorio(Orquestrador *orq, char *novo);
+
+Job* start_tarefa(Orquestrador *orq, char *nome);
+
+void listar_jobs(Orquestrador *orq);
+
+int aguardar_job(Orquestrador *orq, int ID);
+
+int redirecionar(Orquestrador *orq, char *nome_tarefa, char *tipo, char *arquivo);
