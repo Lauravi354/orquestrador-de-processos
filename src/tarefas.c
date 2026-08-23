6f450d89f61783375f4c925b33c1e577962ebbd7
Laura Vitória <lvsa@cesar.school>
2026-08-23 14:05:06 -0300

    Implementar verificar_se_existe()

--- src/tarefas.c
+++ src/tarefas.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include "../include/tarefas.h"
+#include "../include/orquestrador.h"
 
 int verificar_programa_existe(char *caminho_programa){
     if (access(caminho_programa, X_OK) == 0) {
         return 1;
     }
     return 0; 
+}
+
+int verificar_se_existe(Orquestrador *orq, char *nome_tarefa){
+    if (nome_tarefa == NULL) {
+    return 0;
+    }
+    for (int i = 0; i < orq->qtd_tarefas; i++) {
+        if (strcmp(orq->tarefas[i].nome, nome_tarefa) == 0){
+            return 1;
+        }
+    }
+    return 0;
 }
