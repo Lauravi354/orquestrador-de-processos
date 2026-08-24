5c61662f8fb4a6f95a9baedef730a9fe14764dac
Laura Vitória <lvsa@cesar.school>
2026-08-24 17:30:09 -0300

    Criar processos.h

---
+++ include/processos.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fila.h"
+#include "orquestrador.h"
+
+void executar_tarefa_simples(Tarefa *tarefa);
+int executar_sequencial(Orquestrador *orq, char **nomes_tarefas, int qtd);
+int executar_paralelo(Orquestrador *orq, char **nomes_tarefas, int qtd);
+int executar_pipe(Orquestrador *orq, char **nomes_tarefas, int qtd);
