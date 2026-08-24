2e1c7d51228e34c129079a9d7e7017ed83ab6be0
Laura Vitória <lvsa@cesar.school>
2026-08-23 22:01:41 -0300

    Completar tarefas.c e criar jobs.h

---
+++ include/jobs.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fila.h"
+#include "orquestrador.h"
+
+Job* adicionar_job(Orquestrador *orq, int job_id, int pid, char *nome_tarefa);
+
+int remover_job(Orquestrador *orq, int job_id);
+
+Job* buscar_job_por_id(Orquestrador *orq, int job_id);
+
+void listar_jobs(Orquestrador *orq);
+
+int aguardar_job(Orquestrador *orq, int job_id);
--- src/tarefas.c
+++ src/tarefas.c
@@ -23,3 +23,39 @@ int verificar_se_existe(Orquestrador *orq, char *nome_tarefa){
     }
     return 0;
 }
+
+Tarefa* buscar_tarefa_por_nome(Orquestrador *orq, char *nome_tarefa) {
+    if (nome_tarefa == NULL){
+        return NULL;
+    }
+
+    for (int i = 0; i < orq->qtd_tarefas; i++) {
+        if (strcmp(orq->tarefas[i].nome, nome_tarefa) == 0){
+            return &orq->tarefas[i];
+        }
+    }
+    return NULL;
+}
+
+void listar_tarefas(Orquestrador *orq) {
+
+    if (orq == NULL || orq->qtd_tarefas == 0) {
+        printf("Nenhuma tarefa cadastrada.\n");
+        return;
+    }
+
+    for (int i = 0; i < orq->qtd_tarefas; i++) {
+
+        printf("ID: %d\n", orq->tarefas[i].id);
+        printf("Nome: %s\n", orq->tarefas[i].nome);
+        printf("Programa: %s\n", orq->tarefas[i].comando);
+
+        printf("Argumentos: ");
+
+        for (int j = 0; j < orq->tarefas[i].qtd_argumentos; j++) {
+            printf("%s ", orq->tarefas[i].argumentos[j]);
+        }
+
+        printf("\n\n");
+    }
+}
